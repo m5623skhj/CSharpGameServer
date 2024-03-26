a0ad6fe24baf1a5757868bf65371eb9f3cc8e589
m5623skhj <m5623skhj@gmail.com>
2024-03-27 00:01:52 +0900

    * 간단한 테스트용 클라이언트 코드 추가

---
+++ CSharpGameServer/TestClient/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string ip = "127.0.0.1";
+        int port = 10001;
+
+        TcpClient client = new TcpClient(ip, port);
+        NetworkStream stream = client.GetStream();
+
+        string message = "testString";
+        byte[] sendData = Encoding.ASCII.GetBytes(message);
+        stream.Write(sendData, 0, sendData.Length);
+
+        byte[] recvData = new byte[512];
+        StringBuilder builder = new StringBuilder();
+        int recvBytes = stream.Read(recvData, 0, recvData.Length);
+        builder.Append(Encoding.ASCII.GetString(recvData, 0, recvBytes));
+
+        Console.WriteLine("Received : {0}", recvData.ToString());
+
+        stream.Close();
+        client.Close();
+    }
+}
