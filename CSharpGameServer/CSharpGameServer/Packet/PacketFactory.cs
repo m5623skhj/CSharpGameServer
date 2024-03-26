093f7fbe0b3bf0db2c1d9f3284cf4cc2fb39413d
m5623skhj <m5623skhj@gmail.com>
2024-03-27 00:02:33 +0900

    * 패킷 구조체 정의 및 패킷 팩토리 추가

---
+++ CSharpGameServer/CSharpGameServer/Packet/PacketFactory.cs
@@ -0,0 +1,45 @@
+﻿using CSharpGameServer.Protocol;
+
+namespace CSharpGameServer.PacketHandler
+{
+    public class PacketFactory
+    {
+        private static PacketFactory? instance = null;
+        private Dictionary<PacketType, Type> packetTypeDict = new Dictionary<PacketType, Type>();
+
+        public static PacketFactory Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new PacketFactory();
+                }
+
+                return instance;
+            }
+        }
+
+        public void RegisterHandler(PacketType packetType, Type packetObjectType)
+        {
+            packetTypeDict[packetType] = packetObjectType;
+        }
+
+        public Packet? CreatePacket(PacketType packetType)
+        {
+            if (packetTypeDict.TryGetValue(packetType, out Type? packetObjectType) == false)
+            {
+                Console.WriteLine("Invalid packet type {0}", packetType);
+                return null;
+            }
+
+            if (typeof(Packet).IsAssignableFrom(packetObjectType) == false)
+            {
+                Console.WriteLine("Packet type {0} is valid but is not assignable", packetType);
+                return null;
+            }
+
+            return Activator.CreateInstance(packetObjectType) as Packet;
+        }
+    }
+}
---
+++ CSharpGameServer/CSharpGameServer/Protocol/Packet.cs
@@ -0,0 +1,16 @@
+﻿using CSharpGameServer.Core;
+
+namespace CSharpGameServer.Protocol
+{
+    public enum PacketType
+    { 
+        InvalidPacketType = 0,
+    }
+
+    public abstract class Packet
+    {
+        public PacketType type = PacketType.InvalidPacketType;
+
+        public abstract void HandlePacket(Packet thisPacket, Client client);
+    }
+}
