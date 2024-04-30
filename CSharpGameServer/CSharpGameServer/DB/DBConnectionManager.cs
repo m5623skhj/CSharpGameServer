ad4a08dd890408e5bd4d3402a156f5f8ea33d9ea
m5623skhj <m5623skhj@gmail.com>
2024-05-01 03:59:39 +0900

    * MySql.Data 패키지 추가 * DBConnectionManager 추가

---
+++ CSharpGameServer/CSharpGameServer/DB/DBConnectionManager.cs
@@ -0,0 +1,59 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace CSharpGameServer.DB
+{
+    public class DBConnectionManager
+    {
+        private readonly string connectionString;
+        private readonly Queue<MySqlConnection> connectionPool;
+        private readonly int maxPoolSize = 10;
+        private readonly object connectionPoolLock = new object();
+
+        public DBConnectionManager(string server, string db, string userId, string password)
+        {
+            connectionString = $"Server={server};Database={db};Uid={userId};Pwd={password};";
+            connectionPool = new Queue<MySqlConnection>();
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            lock (connectionPoolLock)
+            {
+                if (connectionPool.Count > 0)
+                {
+                    return connectionPool.Dequeue();
+                }
+            }
+
+            return CreateConnection();
+        }
+
+        private MySqlConnection CreateConnection()
+        {
+            var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            return connection;
+        }
+
+        public void ReleaseConnection(MySqlConnection connection)
+        {
+            if (connection == null)
+            {
+                return;
+            }
+
+            lock (connectionPoolLock)
+            {
+                if(connectionPool.Count < maxPoolSize)
+                {
+                    connectionPool.Enqueue(connection);
+                }
+                else
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
