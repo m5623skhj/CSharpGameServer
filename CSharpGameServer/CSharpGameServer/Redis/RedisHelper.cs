3ff5d0608f33b84c5340fb7c80a7c0e5463ceaed
m5623skhj <m5623skhj@gmail.com>
2024-05-22 00:04:15 +0900

    * 이후에 사용하게 될 것 같아 RedisHelper 추가

---
+++ CSharpGameServer/CSharpGameServer/Redis/RedisHelper.cs
@@ -0,0 +1,36 @@
+﻿using StackExchange.Redis;
+
+namespace CSharpGameServer.Redis
+{
+    public class RedisHelper : IDisposable
+    {
+        private readonly ConnectionMultiplexer redis;
+        private readonly IDatabase database;
+
+        public RedisHelper(string connectionString)
+        {
+            redis = ConnectionMultiplexer.Connect(connectionString);
+            database = redis.GetDatabase();
+        }
+
+        public void SetValue(string key, string value)
+        {
+            database.StringAppend(key, value);
+        }
+
+        public string GetValue(string key)
+        {
+            return database.StringGet(key);
+        }
+
+        public bool DeleteKey(string key)
+        {
+            return database.KeyDelete(key);
+        }
+
+        public void Dispose()
+        {
+            redis?.Dispose();
+        }
+    }
+}
