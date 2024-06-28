248039c5014aff8b5c28429fe0289b83e058d733
m5623skhj <m5623skhj@gmail.com>
2024-06-28 23:16:29 +0900

    * SP를 편하게 사용하기 위한 SPBase 추가

---
+++ CSharpGameServer/CSharpGameServer/DB/SPObjects/SPBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+
+namespace CSharpGameServer.DB.SPObjects
+{
+    public class SPBase
+    {
+        protected string? query = null;
+
+        public bool SetParams(object paramObject)
+        {
+            Type paramType = paramObject.GetType();
+            PropertyInfo[] properties = paramType.GetProperties();
+            object[] values = new object[properties.Length];
+
+            for (int i = 0; i < properties.Length; i++)
+            {
+                object? value = properties[i].GetValue(paramObject);
+                if(value == null)
+                {
+                    return false;
+                }
+
+                values[i] = value;
+            }
+
+            return GenerateSPQuery(values);
+        }
+
+        public bool GenerateSPQuery(params object[] values)
+        {
+            if(query == null)
+            {
+                return false;
+            }
+
+            query = string.Format(query, values);
+            return true;
+        }
+    }
+}
