e24a1cfcc2f45d54a52032d75fcd1a632d8f2850
m5623skhj <m5623skhj@gmail.com>
2024-06-28 23:17:10 +0900

    * 예시 오브젝트 추가

---
+++ CSharpGameServer/CSharpGameServer/DB/SPObjects/SPObjectImpl.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpGameServer.DB.SPObjects
+{
+    public class TestSPObject : SPBase
+    {
+        private int? id = null;
+        private string? name = null;
+
+        public TestSPObject() 
+        {
+            query = "SELECT * FROM tbl WHERE id = {0} AND name = \"{1}\"";
+        }
+    }
+}
