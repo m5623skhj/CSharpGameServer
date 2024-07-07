ec7ac20416ab8ea4c44d8191d1fdfd5edf33e5a8
m5623skhj <m5623skhj@gmail.com>
2024-07-07 15:23:21 +0900

    * LazyRunner 추가

---
+++ CSharpGameServer/CSharpGameServer/LazyRunner/LazyRunner.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharpGameServer.LazyRunner
+{
+    public class LazyRunner
+    {
+        private readonly Action action;
+        private readonly int delayMilliSeconds;
+        private Timer timer;
+
+        public LazyRunner(Action inAction, int inDelayMilliSeconds)
+        {
+            action = inAction;
+            delayMilliSeconds = inDelayMilliSeconds;
+            timer = new Timer(Run, null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        public void Start()
+        {
+            timer.Change(delayMilliSeconds, Timeout.Infinite);
+        }
+
+        private void Run(object? _)
+        {
+            action();
+            timer.Dispose();
+        }
+    }
+}
