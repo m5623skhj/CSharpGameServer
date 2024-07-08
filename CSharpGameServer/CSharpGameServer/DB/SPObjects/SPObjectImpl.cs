fdf732899f1bd84b81d51c7cd77786a85f3212a2
m5623skhj <m5623skhj@gmail.com>
2024-07-09 00:09:28 +0900

    * SP 후속 처리를 위해 Owner를 PC로 가질 수 있는 PCOwnerSPObject 추가 ** OnCommit(), OnRollback() 강제로 구현하도록 추상 함수로 정의

--- CSharpGameServer/CSharpGameServer/DB/SPObjects/SPObjectImpl.cs
+++ CSharpGameServer/CSharpGameServer/DB/SPObjects/SPObjectImpl.cs
@@ -1,13 +1,39 @@
 ﻿namespace CSharpGameServer.DB.SPObjects
 {
-    public class TestSPObject : SPBase
+    public abstract class PCOwnerSPObject : SPBase
+    {
+        protected PC.PC? owner = null;
+
+        public PCOwnerSPObject(PC.PC inOwner) 
+        {
+            owner = inOwner;
+        }
+
+        public abstract void OnCommit();
+        public abstract void OnRollback();
+    }
+
+    public class TestSPObject : PCOwnerSPObject
     {
         private int? id = null;
         private string? name = null;
 
-        public TestSPObject() 
+        public TestSPObject(PC.PC inOwner) 
+            : base(inOwner)
         {
             query = "SELECT * FROM tbl WHERE id = {0} AND name = \"{1}\"";
+        }
+
+        public override void OnCommit()
+        {
+            // checkt if owner is not null then
+            // Callback owner TestSPObject on committed
+        }
+
+        public override void OnRollback()
+        {
+            // check if owner is not null then
+            // Callback owner TestSPObject on rollbacked
         }
     }
 }
