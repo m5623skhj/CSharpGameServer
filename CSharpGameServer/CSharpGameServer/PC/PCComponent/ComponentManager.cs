154144522bc21c4b34f4c9bf007701ac9d47db02
m5623skhj <m5623skhj@gmail.com>
2024-07-15 00:01:14 +0900

    * 확장 메서드를 사용하여 GetValueOrNull 추가 * ComponentManager가 Component Dictionary를 가지고, 외부에서는 이를 통하여 관리 및 참조하도록 수정

--- CSharpGameServer/CSharpGameServer/PC/PCComponent/ComponentManager.cs
+++ CSharpGameServer/CSharpGameServer/PC/PCComponent/ComponentManager.cs
@@ -1,10 +1,34 @@
 ﻿namespace CSharpGameServer.PC.PCComponent
 {
+    public enum ComponentType : int
+    {
+        InvalidComponentType = 0,
+    }
+
     public class ComponentManager
     {
+        private Dictionary<ComponentType, ComponentBase> components = new Dictionary<ComponentType, ComponentBase>();
+
+        private void AddComponentListForInitialize()
+        {
+            components.Clear();
+
+            // AddComponent
+        }
+
         public void Initialize()
         {
+            AddComponentListForInitialize();
 
+            foreach (var component in components.Values)
+            {
+                component.Initialize();
+            }
+        }
+
+        public ComponentBase? GetComponent(ComponentType inComponentType)
+        {
+            return components.GetValueOrNull(inComponentType);
         }
     }
 
---
+++ CSharpGameServer/CSharpGameServer/etc/DictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿public static class DictionaryExtensions
+{
+    public static TValue? GetValueOrNull<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key) where TValue : class
+    {
+        if (dictionary.TryGetValue(key, out TValue value))
+        {
+            return value;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
