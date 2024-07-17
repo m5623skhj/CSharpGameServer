7ad093e7eecf56ab26c5eba19745af62f3c28d99
m5623skhj <m5623skhj@gmail.com>
2024-07-18 00:06:24 +0900

    * Main에서 코어를 직접 다루지 않도록 수정 ** 해당 부분을 ServerBase라는 별도의 추상 클래스로 뽑아내고, 다른 서버에서는 이를 상속하여 사용할 수 있도록 수정

---
+++ CSharpGameServer/CSharpGameServer/Core/ServerBase.cs
@@ -0,0 +1,41 @@
+﻿using CSharpGameServer.Logger;
+
+namespace CSharpGameServer.Core
+{
+    public abstract class ServerBase
+    {
+        protected void Run(string serverName)
+        {
+            ServerCore.Instance.Run();
+            LoggerManager.Instance.WriteLogDebug("------------ " + serverName + " Server running ------------");
+            ServerRunning();
+
+            ServerCore.Instance.Stop();
+            LoggerManager.Instance.WriteLogDebug("------------ " + serverName + " Server stopped ------------");
+        }
+
+        protected void ServerRunning()
+        {
+            bool running = true;
+            while (running)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            {
+                                running = false;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ CSharpGameServer/CSharpGameServer/GameServer/GameServer.cs
@@ -0,0 +1,14 @@
+﻿using CSharpGameServer.Core;
+
+namespace CSharpGameServer.GameServer
+{
+    public class GameServer : ServerBase
+    {
+        string serverName = "GameServer";
+
+        public void Run()
+        {
+            Run(serverName);
+        }
+    }
+}
