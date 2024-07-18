090661d7f24838793bfa2e82528355c6df03b4fc
m5623skhj <m5623skhj@gmail.com>
2024-07-19 00:00:42 +0900

    * 패킷 자동 생성을 위한 제네레이터 툴과 유저가 패킷 이름을 정의할 yml 파일 추가 ** 아직 완성되진 않아서 임시 커밋임

---
+++ CSharpGameServer/PacketGeneratorTool.py
@@ -0,0 +1,86 @@
+import yaml
+
+def GenerateEnumValue(packetName, values):
+    enumCode = f"enum {packetName}\n{{\n"
+    for value in values:
+        enumCode += f"  {value},\n"
+    enumCode += "}\n"
+    
+    return enumCode
+        
+def GenerateProtocolOverride(packetName):
+    method_name = packetName + "Handler"
+    return f"""
+    public override void {method_name}()
+    {{
+        // TODO: Add your implementation here.
+    }}
+    """
+
+def GeneratePacketHandler(packetName):
+    method_name = packetName + "Handler"
+    return f"""
+    public void {method_name}()
+    {{
+        // TODO: Handle packet
+    }}
+    """
+
+def GenerateClientPacketHandler(packetName):
+    method_name = packetName + "Handler"
+    return f"""
+    public override void {method_name}()
+    {{
+        base.{method_name}();
+        // TODO: Add client-specific handling here.
+    }}
+    """
+
+def GeneratePCPacketHandler(packetName):
+    method_name = packetName + "Handler"
+    return f"""
+    public override void {method_name}()
+    {{
+        // TODO: Implement the method.
+    }}
+    """
+
+def ProcessPacketGenerate():
+    with open(ymlFilePath, 'r') as file:
+        ymlData = yaml.load(file, Loader=yaml.SafeLoader)
+        
+    # Generate PacketType.cs
+    enumCode = GenerateEnumValue('PacketType', ymlData['Packet'])
+    with open(packetTypeFilePath, 'w') as file:
+        file.write(enumCode)
+    
+    # Generate Protocol.cs
+    with open(protocolFilePath, 'w') as file:
+        for packetName in ymlData['Packet']:
+            file.write(GenerateProtocolOverride(packetName))
+    
+    # Generate PacketHandler.cs
+    with open(packetHandlerFilePath, 'w') as file:
+        for packetName in ymlData['Packet']:
+            file.write(GeneratePacketHandler(packetName))
+            
+    # Generate ClientPacketHandler.cs
+    with open(packetHandlerFilePath, 'w') as file:
+        for packetName in ymlData['Packet']:
+            file.write(GenerateClientPacketHandler(packetName))
+    
+    # Generate PCPacketHandler.cs
+    with open(packetHandlerFilePath, 'w') as file:
+        for packetName in ymlData['Packet']:
+            file.write(GeneratePCPacketHandler(packetName))
+
+# Write file path here
+packetTypeFilePath = 'CSharpGameServer/PacketType.cs'
+protocolFilePath = 'CSharpGameServer/Protocol.cs'
+packetHandlerFilePath = 'CSharGameServer/PacketHandler.cs'
+clientPacketHandlerFilePath = 'CSharpGameServer/Core/ClientManager.cs'
+pcPacketHandlerFilePath = 'CSharpGameServer/PC/PCPacketHandler.cs'
+ymlFilePath = 'PacketDefine.yml'
+
+ProcessPacketGenerate()
+print("Code generated successfully")
