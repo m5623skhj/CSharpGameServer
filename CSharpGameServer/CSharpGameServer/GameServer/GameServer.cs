d55e8dd81424af3c336147ae6ed0af68f63b5d41
m5623skhj <m5623skhj@gmail.com>
2024-07-29 00:14:09 +0900

    * Migration을 Main에서 GameServer로 이관

--- CSharpGameServer/CSharpGameServer/GameServer/GameServer.cs
+++ CSharpGameServer/CSharpGameServer/GameServer/GameServer.cs
@@ -1,13 +1,27 @@
 ﻿using CSharpGameServer.Core;
+using CSharpGameServer.DB.Migration;
 
 namespace CSharpGameServer.GameServer
 {
     public class GameServer : ServerBase
     {
-        string serverName = "GameServer";
+        private const int migrationSuccessResult = 1;
+        private string serverName = "GameServer";
+
+        private static bool IsMigrationSuccess(int migrationResult)
+        {
+            return migrationResult == migrationSuccessResult;
+        }
 
         public void Run()
         {
+            Console.WriteLine("------------ Try migration ------------");
+            if (IsMigrationSuccess(MigrationRunner.Instance.RunMigration()) == false)
+            {
+                Console.WriteLine("------------ Migration failed ------------");
+            }
+            Console.WriteLine("------------ Migration succeded ------------");
+
             Run(serverName, new GameServerCore());
         }
     }
--- CSharpGameServer/CSharpGameServer/Main/Main.cs
+++ CSharpGameServer/CSharpGameServer/Main/Main.cs
@@ -1,23 +1,10 @@
-﻿using CSharpGameServer.DB.Migration;
-using CSharpGameServer.GameServer;
+﻿using CSharpGameServer.GameServer;
 
 class Program
 {
-    private static bool IsMigrationSuccess(int migrationResult)
-    {
-        return migrationResult == 1;
-    }
-
     static void Main(string[] args)
     {
-        Console.WriteLine("------------ Try migration ------------");
-        if (IsMigrationSuccess(MigrationRunner.Instance.RunMigration()) == false)
-        {
-            Console.WriteLine("------------ Migration failed ------------");
-        }
-        Console.WriteLine("------------ Migration succeded ------------");
-
         GameServer gameServer = new GameServer();
         gameServer.Run();
-   }
+    }
 }
